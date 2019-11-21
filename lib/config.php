9e9b922e76b9ab608ba64b170f2249249863c6f6
JS Morisset <jsm@surniaulula.com>
2019-11-21 14:12:26 -0800

    v4.4.1-dev.2

--- lib/config.php
+++ lib/config.php
@@ -16,7 +16,7 @@ if ( ! class_exists( 'WpssoPlmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoplm' => array(			// Plugin acronym.
-					'version'     => '4.4.0',	// Plugin version.
+					'version'     => '4.4.1-dev.2',	// Plugin version.
 					'opt_version' => '24',		// Increment when changing default option values.
 					'short'       => 'WPSSO PLM',	// Short plugin name.
 					'name'        => 'WPSSO Place / Location and Local SEO Meta',
@@ -29,7 +29,7 @@ if ( ! class_exists( 'WpssoPlmConfig' ) ) {
 					'req'         => array(
 						'short'       => 'WPSSO Core',
 						'name'        => 'WPSSO Core',
-						'min_version' => '6.12.0',
+						'min_version' => '6.13.0-dev.2',
 					),
 					'assets' => array(
 						'icons' => array(
--- lib/register.php
+++ lib/register.php
@@ -99,11 +99,14 @@ if ( ! class_exists( 'WpssoPlmRegister' ) ) {
 
 			if ( class_exists( 'Wpsso' ) ) {
 
-				if ( class_exists( 'WpssoUtil' ) ) {	// Just in case.
+				/**
+				 * Register plugin install, activation, update times.
+				 */
+				if ( class_exists( 'WpssoUtilReg' ) ) {	// Since WPSSO v6.13.0.
 
 					$version = WpssoPlmConfig::$cf[ 'plugin' ][ 'wpssoplm' ][ 'version' ];
 
-					WpssoUtil::register_ext_version( 'wpssoplm', $version );
+					WpssoUtilReg::update_ext_version( 'wpssoplm', $version );
 				}
 			}
 		}

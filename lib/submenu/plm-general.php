658c73b77e8ed93587fe8dad7b7c3f2b897ae99d
JS Morisset <jsm@surniaulula.com>
2020-06-03 07:06:37 -0700

    v4.14.0-dev.5

--- lib/submenu/plm-general.php
+++ lib/submenu/plm-general.php
@@ -314,7 +314,7 @@ if ( ! class_exists( 'WpssoPlmSubmenuPlmGeneral' ) && class_exists( 'WpssoAdmin'
 
 					$add_to_checkboxes = '';
 
-					foreach ( $this->p->util->get_post_types( 'objects' ) as $obj ) {
+					foreach ( SucomUtilWP::get_post_types( 'objects' ) as $obj ) {
 
 						$add_to_checkboxes .= '<p>' .
 							$this->form->get_checkbox( 'plm_add_to_' . $obj->name ) . ' ' .

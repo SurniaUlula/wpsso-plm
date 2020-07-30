850b6348ac2e80e07e214527185f78b53aa6f8ca
JS Morisset <jsm@surniaulula.com>
2020-07-30 13:19:00 -0700

    v4.15.1

--- lib/gettext-config.php
+++ lib/gettext-config.php
@@ -3,6 +3,8 @@
 /**
  * Extract of translatable text strings from the static config array.
  */
+_x( 'Extend Post Edit Settings', 'lib file description', 'wpsso-plm' );
+_x( 'Extend Post Edit Settings', 'lib file description', 'wpsso-plm' );
 _x( 'Places', 'lib file description', 'wpsso-plm' );
 _x( 'Manage Places and Local SEO for Facebook / Open Graph, Pinterest, Google Local Business and Schema Place.', 'plugin description', 'wpsso-plm' );
 _x( 'local.business', 'option value', 'wpsso-plm' );

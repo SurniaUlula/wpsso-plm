db0601ffc5c7eda0331ffddba9a6fc1aeeec0567
JS Morisset <jsm@surniaulula.com>
2020-07-31 11:06:25 -0700

    v4.16.0-dev.2

--- wpsso-plm.php
+++ wpsso-plm.php
@@ -14,7 +14,7 @@
  * Requires PHP: 5.6
  * Requires At Least: 4.2
  * Tested Up To: 5.5
- * Version: 4.15.1
+ * Version: 4.16.0-dev.2
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *

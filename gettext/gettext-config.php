72967ab52950234ab93127051228d72c0c4538e5
JS Morisset <jsm@surniaulula.com>
2020-09-13 11:02:25 -0700

    v4.17.1

---
+++ gettext/gettext-config.php
@@ -0,0 +1,25 @@
+<?php die( 'These aren\'t the droids you\'re looking for.' );
+
+/**
+ * Extract of translatable strings from the config array.
+ */
+_x( 'Places', 'lib file description', 'wpsso-plm' );
+_x( 'Manage Places and Local SEO for Facebook / Open Graph, Pinterest, Google Local Business and Schema Place.', 'plugin description', 'wpsso-plm' );
+_x( 'local.business', 'option value', 'wpsso-plm' );
+_x( 'wp', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );
+_x( '09:00', 'option value', 'wpsso-plm' );
+_x( '17:00', 'option value', 'wpsso-plm' );

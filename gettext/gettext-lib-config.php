a3d78bbe54725b39b8c111bce298aafed84fcd25
JS Morisset <jsm@surniaulula.com>
2020-09-15 09:17:43 -0700

    v4.17.1

--- gettext/gettext-lib-config.php
+++ gettext/gettext-lib-config.php
@@ -4,6 +4,7 @@
  * Extract of translatable strings from lib/config.php.
  */
 _x( 'Places', 'lib file description', 'wpsso-plm' );
+_x( 'WPSSO Place and Local SEO Markup', 'plugin name', 'wpsso-plm' );
 _x( 'Manage Places and Local SEO for Facebook / Open Graph, Pinterest, Google Local Business and Schema Place.', 'plugin description', 'wpsso-plm' );
 _x( 'local.business', 'option value', 'wpsso-plm' );
 _x( 'wp', 'option value', 'wpsso-plm' );

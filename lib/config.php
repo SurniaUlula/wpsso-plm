849ae91b4576401e23dc8733d1b9ab8d1d919794
JS Morisset <jsm@surniaulula.com>
2020-10-01 19:12:57 -0700

    v4.18.0-dev.3

--- lib/config.php
+++ lib/config.php
@@ -17,7 +17,7 @@ if ( ! class_exists( 'WpssoPlmConfig' ) ) {
 		public static $cf = array(
 			'plugin' => array(
 				'wpssoplm' => array(			// Plugin acronym.
-					'version'     => '4.18.0-dev.2',	// Plugin version.
+					'version'     => '4.18.0-dev.3',	// Plugin version.
 					'opt_version' => '26',		// Increment when changing default option values.
 					'short'       => 'WPSSO PLM',	// Short plugin name.
 					'name'        => 'WPSSO Place and Local SEO Markup',
@@ -37,7 +37,7 @@ if ( ! class_exists( 'WpssoPlmConfig' ) ) {
 							'home'          => 'https://wordpress.org/plugins/wpsso/',
 							'plugin_class'  => 'Wpsso',
 							'version_const' => 'WPSSO_VERSION',
-							'min_version'   => '8.7.0-dev.2',
+							'min_version'   => '8.7.0-dev.3',
 						),
 					),
 

550b39379699c33d4a21d98601a14089256e3dd6
JS Morisset <jsm@surniaulula.com>
2021-05-12 20:05:48 -0700

    v5.3.0

--- wpsso-plm.php
+++ wpsso-plm.php
@@ -13,7 +13,7 @@
  * Description: Manage Schema Places and Local SEO for Facebook / Open Graph, Pinterest, and Google Local Business.
  * Requires PHP: 7.0
  * Requires At Least: 4.5
- * Tested Up To: 5.7.1
+ * Tested Up To: 5.7.2
  * Version: 5.3.0
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]

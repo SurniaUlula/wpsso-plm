af32dfda56c70c14138dff1cb91a6fbef9627952
JS Morisset <jsm@surniaulula.com>
2021-06-07 06:28:58 -0700

    v5.4.0-b.1

--- wpsso-plm.php
+++ wpsso-plm.php
@@ -14,7 +14,7 @@
  * Requires PHP: 7.0
  * Requires At Least: 4.5
  * Tested Up To: 5.7.2
- * Version: 5.4.0-dev.4
+ * Version: 5.4.0-b.1
  * 
  * Version Numbering: {major}.{minor}.{bugfix}[-{stage}.{level}]
  *
